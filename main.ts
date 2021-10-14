6c7d200dfe4b89267ba436a822df7abdbd14ea97
Joshua McLean <jayshoo@gmail.com>
2021-10-14 23:04:40 +1100

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,13 +6,20 @@ async function measuringStick(id: string): Promise<Response> {
   let hash = await crypto.subtle.digest('SHA-256', data)
   let view = new DataView(hash)
   
-  let answer = [
+  let x = [
     view.getUint8(0),
     view.getUint8(1),
     view.getUint8(2)
   ]
+  let n = [
+    x[0] / 256 * 11 + 1,
+    x[1] / 256 * 4 + 1,
+    x[2] / 256 * 60
+  ]
+
+  let answer = `Dick length is ${n[0].toFixed(1)} inch and has a width of ${n[1].toFixed(0)} inch, with an angle of ${n[2].toFixed(0)} degrees.`
   
-  return new Response(JSON.stringify(answer))
+  return new Response(answer)
 }
 
 addEventListener('fetch', event => {
